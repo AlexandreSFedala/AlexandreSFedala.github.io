742f0ffc43039431ea26a555fab4e5fa1eab0e05
AlexandreSFedala <alexandrefedala.afedala@gmail.com>
2025-09-15 00:16:03 +0100

    Update particles-init.js

--- particles-init.js
+++ particles-init.js
@@ -1,17 +1,17 @@
 // particles-init.js
-particlesJS('particles-js', {
+tsParticles.load('particles-js', {
   particles: {
-    number: { value: 60, density: { enable: true, value_area: 800 } },
+    number: { value: 40, density: { enable: true, value_area: 800 } },
     color: { value: '#f0f0f0' },
     shape: { type: 'circle' },
     opacity: { value: 0.9 },
-    size: { value: 5, random: true },
+    size: { value: 20, random: true },
     line_linked: {
       enable: true,
-      distance: 160,
+      distance: 250,
       color: '#e6a21a',
       opacity: 0.6,
-      width: 1
+      width: {min:4, max:10},
     },
     move: { enable: true, speed: 2, out_mode: 'out' }
   },
