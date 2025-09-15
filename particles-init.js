31bc386716a4d6aa0753258b0830aee34f444765
AlexandreSFedala <alexandrefedala.afedala@gmail.com>
2025-09-15 12:45:33 +0100

    Update particles-init.js

--- particles-init.js
+++ particles-init.js
@@ -1,7 +1,7 @@
 // particles-init.js
 particlesJS('particles-js', {
   particles: {
-    number: { value: 40, density: { enable: true, value_area: 800 } },
+    number: { value: 20, density: { enable: true, value_area: 800 } },
     color: { value: '#f0f0f0' },
     shape: { type: 'circle' },
     opacity: { value: 0.9 },
@@ -13,7 +13,7 @@ particlesJS('particles-js', {
       opacity: 0.6,
       width: 3
     },
-    move: { enable: true, speed: 4, out_mode: 'out' }
+    move: { enable: true, speed: 3, out_mode: 'out' }
   },
   interactivity: {
     detect_on: 'window',
@@ -23,7 +23,7 @@ particlesJS('particles-js', {
       resize: true
     },
     modes: {
-      grab: { distance: 500, line_linked: { opacity: 1 } },
+      grab: { distance: 400, line_linked: { opacity: 1 } },
       push: { particles_nb: 4 }
     }
   },
